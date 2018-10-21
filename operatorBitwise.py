255fd1d190944a37951c27d06933b4339c3e7c5a
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-21 13:22:02 +0530

    Add files via upload

---
+++ operatorBitwise.py
@@ -0,0 +1,8 @@
+a=10
+b=4
+print('Bitwise AND(&) operator:',a&b)
+print('Bitwise OR(|) operator:',a|b)
+print('Bitwise XOR(^) operator:',a^b)
+print('Bitwise NOT(~) operator:',~a)
+print('Bitwise RightShift(>>) operator:',a>>b)
+print('Bitwise LeftShift(<<) operator:',a<<b)
