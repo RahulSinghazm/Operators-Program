d734be32a3d41bce21913c744b127fad8b7506cc
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-21 13:37:30 +0530

    Add files via upload

---
+++ operatorSpecial.py
@@ -0,0 +1,41 @@
+a=1000
+print(a)
+print(id(a))
+
+b=2000
+print(b)
+print(id(b))
+print(a is b)
+print(a is not b)
+
+c=3000
+print(c)
+print(id(c))
+
+d=3000
+print(d)
+print(id(d))
+print(c is d)
+print(c is not d)
+
+e=[10,20,30]
+print(e)
+print(id(e))
+
+f=[10,20,30]
+print(f)
+print(id(f))
+print(e is f)
+print(e is not f)
+
+g=[10,20,30]
+print(g)
+print(id(g))
+
+g=[10,20,30]
+print(g)
+print(id(g))
+print(g is g)
+print(g is not g)
+
+
